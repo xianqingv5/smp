aa16d4b76a241a6f0d504d6032e1ccf062407998
lvjie1 <lvjie1@yiche.com>
2018-03-19 17:03:24 +0800

    工具类

--- src/main/java/com/yiche/smp/common/ErrorCodeMessage.java
+++ src/main/java/com/yiche/smp/common/ErrorCodeMessage.java
@@ -4,7 +4,7 @@ package com.yiche.smp.common;
  * 使用枚举类型来封装异常码和异常信息
  */
 public enum ErrorCodeMessage {
-    APPLY_MESSAGE_ADD_ERROR(1000, "申请信息提交申请失败"), APPLY_MESSAGE_CHECK_ERROR(1001, "审核失败"), DB_SERVICE_ERROR(9999, "服务器异常"), DB_SERVICE_UNKNOWN_ERROR(20101, "未知异常"), DB_SERVICE_NETWORK_ERROR(20103, "网络异常"), DB_SERVICE_INVALID_PARAMETER(20106, "方法参数错误"), DB_SERVICE_FUNCTION_NO_ACCESS(20107, "对此方法无访问权限"), DB_SERVICE_GET_USER_MESSAGE_ERROR(20108, "没有获取到用户登录信息,无访问权限");
+    APPLY_MESSAGE_ADD_ERROR(1000, "申请信息提交申请失败"), APPLY_MESSAGE_CHECK_ERROR(1001, "审核失败"), DB_SERVICE_ERROR(9999, "服务器异常"), DB_SERVICE_UNKNOWN_ERROR(20101, "未知异常"), DB_SERVICE_NETWORK_ERROR(20103, "网络异常"), DB_SERVICE_INVALID_PARAMETER(20106, "方法参数错误"), DB_SERVICE_FUNCTION_NO_ACCESS(20107, "对此方法无访问权限"), DB_SERVICE_GET_USER_MESSAGE_ERROR(20108, "没有获取到用户登录信息,无访问权限"),FILEUPLOAD_ERROR(20109,"文件上传失败"),FILEWRITE_ERROR(20110,"数据入库失败");
     private String msg;
     private int code;
 
