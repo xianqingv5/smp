fbb19bd0afbec11d985312507e027b46d1c9d355
lvjie1 <lvjie1@yiche.com>
2018-03-21 16:11:52 +0800

    update

--- src/main/java/com/yiche/smp/common/DataName.java
+++ src/main/java/com/yiche/smp/common/DataName.java
@@ -1,7 +1,7 @@
 package com.yiche.smp.common;
 
 public enum DataName {
-	NameError(1,"平台名称无效"),DateError(2,"日期无效"),UserError(3,"不是有效的易车域账号"),DiffError(4,"请正确填写数据归属人帐号"),NobelongError(5,"请上传自己的数据文件"),ChooseError(6,"请选择正确的日期上传"),FileError(7,"文件内日期时间错误"),FileFormatError(8,"文件格式错误");
+	NameError(1,"平台名称无效"),DateError(2,"日期无效"),UserError(3,"不是有效的易车域账号"),DiffError(4,"请正确填写数据归属人帐号"),NobelongError(5,"请选择代上传人的数据文件"),ChooseError(6,"请选择正确的日期上传"),FileError(7,"文件内日期时间错误"),FileFormatError(8,"文件格式错误");
 	private int code;
 	private String msg;
 	public int getCode() {
