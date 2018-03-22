1651c8c42712d07b22566ca6160b6be11de696f2
lvjie1 <lvjie1@yiche.com>
2018-03-22 11:38:46 +0800

    下载

--- src/main/java/com/yiche/smp/common/util/UploadDataUtil.java
+++ src/main/java/com/yiche/smp/common/util/UploadDataUtil.java
@@ -1,11 +1,15 @@
 package com.yiche.smp.common.util;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -37,4 +41,27 @@ public class UploadDataUtil {
 	        map.put("filename", originalFilename);
 	        return map;
 	    }
+	
+	 //下载时读取文件到流中
+    public static void processDownload(HttpServletResponse response, String filePath, String fileName) {
+    	 try {
+            FileInputStream inputStream = new FileInputStream(filePath);
+            //设置头信息
+            response.reset();
+            response.setContentType("bin");
+            response.addHeader("Content-Disposition", "attachment; filename=\"" + URLEncoder.encode(fileName, "UTF-8") + "\"");
+            //循环取流中的数据
+            byte[] b = new byte[100];
+            int len;
+            while ((len = inputStream.read(b)) > 0) {
+                response.getOutputStream().write(b, 0, len);
+            }          
+				inputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				return;
+			}
+        
+    }
+	
 }
